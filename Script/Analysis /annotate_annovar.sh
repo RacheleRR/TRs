5129398bcb643c6e5ed03fbe5c53fe18204cf531
RacheleRR <rachelerebeccarubiu98@gmail.com>
2025-02-21 18:10:54 +0100

    added the one for with and without QC

--- Script/Analysis /annotate_annovar.sh
+++ Script/Analysis /annotate_annovar.sh
@@ -2,5 +2,5 @@
 
 #annoate the EHDN results 
 
-/home/rachele/programs/ExpansionHunterDenovo/scripts/annotate_ehdn.sh --ehdn-results /home/rachele/ehdn_DBSCAN_reorder.tsv --ehdn-annotated-results /home/rachele/ehdn_DBSCAN_annotated.tsv --annovar-annotate-variation /home/rachele/programs/ExpansionHunterDenovo/scripts/annovar/annotate_variation.pl --annovar-humandb /home/rachele/programs/ExpansionHunterDenovo/scripts/annovar/humandb --annovar-buildver hg38
+/home/rachele/programs/ExpansionHunterDenovo/scripts/annotate_ehdn.sh --ehdn-results /home/rachele/EHDN_DBSCAN_correct/Result/results_dbscan_without_QC_NOUHR/AFTER_DBSCAN/ehdn_DBSCAN_reorder.tsv --ehdn-annotated-results /home/rachele/EHDN_DBSCAN_correct/Result/results_dbscan_without_QC_NOUHR/AFTER_DBSCAN/ehdn_DBSCAN_annotated.tsv --annovar-annotate-variation /home/rachele/programs/ExpansionHunterDenovo/scripts/annovar/annotate_variation.pl --annovar-humandb /home/rachele/programs/ExpansionHunterDenovo/scripts/annovar/humandb --annovar-buildver hg38
 
