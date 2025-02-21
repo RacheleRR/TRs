954a460fbb39bcc618909a3280dbf6beef32da0b
RacheleRR <rachelerebeccarubiu98@gmail.com>
2025-02-21 12:13:24 +0100

    script to annotate with annovar

---
+++ Script/Analysis /annotate_annovar.sh
@@ -0,0 +1,6 @@
+
+
+#annoate the EHDN results 
+
+/home/rachele/programs/ExpansionHunterDenovo/scripts/annotate_ehdn.sh --ehdn-results /home/rachele/ehdn_DBSCAN_reorder.tsv --ehdn-annotated-results /home/rachele/ehdn_DBSCAN_annotated.tsv --annovar-annotate-variation /home/rachele/programs/ExpansionHunterDenovo/scripts/annovar/annotate_variation.pl --annovar-humandb /home/rachele/programs/ExpansionHunterDenovo/scripts/annovar/humandb --annovar-buildver hg38
+
