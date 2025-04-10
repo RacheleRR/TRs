78c9a94dbbecde18a4268c164267e534a9606550
RacheleRR <124870594+RacheleRR@users.noreply.github.com>
2025-04-11 01:08:28 +0200

    Delete Script/Analysis  directory

--- Script/Analysis /annotate_annovar.sh
+++
@@ -1,7 +0,0 @@
-
-
-#annoate the EHDN results 
-
-/home/rachele/programs/ExpansionHunterDenovo/scripts/annotate_ehdn.sh --ehdn-results /home/rachele/EHDN_DBSCAN_correct/Result/results_dbscan_without_QC_NOUHR/AFTER_DBSCAN/ehdn_DBSCAN_reorder.tsv --ehdn-annotated-results /home/rachele/EHDN_DBSCAN_correct/Result/results_dbscan_without_QC_NOUHR/AFTER_DBSCAN/ehdn_DBSCAN_annotated.tsv --annovar-annotate-variation /home/rachele/programs/ExpansionHunterDenovo/scripts/annovar/annotate_variation.pl --annovar-humandb /home/rachele/programs/ExpansionHunterDenovo/scripts/annovar/humandb --annovar-buildver hg38
-
-/home/rachele/programs/ExpansionHunterDenovo/scripts/annotate_ehdn.sh --ehdn-results /home/rachele/EHDN_DBSCAN_correct/Result/results_dbscan_with_QC_NOUHR/AFTER_DBSCAN/ehdn_DBSCAN_reorder.tsv --ehdn-annotated-results /home/rachele/EHDN_DBSCAN_correct/Result/results_dbscan_with_QC_NOUHR/AFTER_DBSCAN/ehdn_DBSCAN_annotated.tsv --annovar-annotate-variation /home/rachele/programs/ExpansionHunterDenovo/scripts/annovar/annotate_variation.pl --annovar-humandb /home/rachele/programs/ExpansionHunterDenovo/scripts/annovar/humandb --annovar-buildver hg38
